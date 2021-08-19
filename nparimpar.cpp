adb73b9904fb4f6652f9686ca6aab46c7724d7f9
Daniel Garrocho <danmgap@gmail.com>
2021-08-18 22:17:05 -0300

    18/08

---
+++ nparimpar.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<locale.h>
+   
+
+int main (){
+    int i=0,k,n,par,impar;
+    setlocale(LC_ALL,"Portuguese");
+    printf("Digite quantas interações o algoritmo deverá fazer: \n");
+    scanf("%d",&n);
+    while (i<n){
+        printf("Digite um número a ser verificado se é par ou impar: \n");
+        scanf("%d",&k);
+        if (k%2==0 || k==0){par+=1;}
+        if (k%2!=0){impar+=1;}
+    }
+    printf("Há %d números pares e %d números ímpares.",par,impar);
+}
