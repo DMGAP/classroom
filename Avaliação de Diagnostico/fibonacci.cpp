221553fbee8d7e46cf5b8c8aa82f9447826484c5
Daniel Garrocho <danmgap@gmail.com>
2021-08-20 20:16:45 -0300

    20/08

--- Avaliação de Diagnostico/fibonacci.cpp
+++ Avaliação de Diagnostico/fibonacci.cpp
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include<locale.h>
 void fibonacci (){
-    int v[3],k,i=2;
+    double v[3];
+    int k,i=2;
     v[0]=v[1]=1;
     scanf("%d",&k);
-    if (k==1){printf("%d",v[0]);}
-    if (k==2){printf("%d, %d",v[0],v[1]);}
+    if (k==1){printf("%.lf",v[0]);}
+    if (k==2){printf("%.lf, %.lf",v[0],v[1]);}
     if (k>2) { 
-        printf("%d",v[0]);
-        printf(", %d",v[0]);
+        printf("%.lf",v[0]);
+        printf(", %.lf",v[0]);
         while(i<k){
             v[2]=v[0]+v[1];
-            printf(", %d",v[2]);
+            printf(", %.lf",v[2]);
             v[0]=v[1];
             v[1]=v[2];
             i++;
